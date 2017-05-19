e68b8a7e6b8ddca85db061aa4fe7338af89ddbc4
qianqian121 <qianqian141@hotmail.com>
2017-05-19 11:47:36 -0700

    code review8
    The process of normalising an angle appears multiple times in your code. It would be great to see this repetition refactored into helper functions (perhaps kept in the Tools class).
    
    This refactoring would allow you to easily swap out the normalisation code for a different process that doesn't use while loops (such as using fmod or atan2).

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -408,8 +408,7 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
     VectorXd z_diff = Zsig.col(i) - z_pred;
 
     //angle normalization
-    while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
-    while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+    z_diff(1) = fmod(z_diff(1), 2.*M_PI);
 
     S = S + weights_(i) * z_diff * z_diff.transpose();
   }
@@ -431,14 +430,12 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
     //residual
     VectorXd z_diff = Zsig.col(i) - z_pred;
     //angle normalization
-    while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
-    while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+    z_diff(1) = fmod(z_diff(1), 2.*M_PI);
 
     // state difference
     VectorXd x_diff = Xsig_pred_.col(i) - x_;
     //angle normalization
-    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
-    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+    x_diff(3) = fmod(x_diff(3), 2.*M_PI);
 
     Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
   }
