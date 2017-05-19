9e840c89f6a8512ebc8d16f34402b8c00995e4fd
qianqian121 <qianqian141@hotmail.com>
2017-05-19 10:34:23 -0700

    code review4
    Lines 237-240 could be replaced with the following vectorised operation:
    
    x_ = Xsig_pred_ * weights_;
    Although array programming is not native to C++, Eigen does provide these operations for matrix and vector calculations. Here is a helpful description of array programming.
    https://en.wikipedia.org/wiki/Array_programming
    Some developers may consider array programming to be less readable and avoid using it even if it is more efficient. It's fine if you agree with this, but I wanted to bring it up in case you weren't aware of this approach.
    
    There are other opportunities to use vectorised operations in your code, but I'll leave it as an exercise for you to refactor your project if you wish to.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -232,9 +232,7 @@ void UKF::Prediction(double delta_t) {
 
   //predicted state mean
   x_.fill(0.0);
-  for (int i = 0; i < n_sig_; i++) {  //iterate over sigma points
-    x_ = x_ + weights_(i) * Xsig_pred_.col(i);
-  }
+  x_ = Xsig_pred_ * weights_;
   
   //predicted state covariance matrix
   P_.fill(0.0);
